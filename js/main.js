8e0aec026e47a5131dc9388cc1b73b2c84bc034c
Bondar Anton <bondar8524567913@gmail.com>
2024-03-18 14:05:45 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("This is a log message");
+console.info("This is  an inform message");
+console.warn("This is a warning message");
+console.error("This is an error message")
+
+
+const name = "Anton";
+
+console.info(name);
