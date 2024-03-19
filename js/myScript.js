b07ee3c73387bea7d81cce48891825b7c5847427
Bondar Anton <bondar8524567913@gmail.com>
2024-03-19 20:51:09 +0100

    Collection sorting complete

--- js/myScript.js
+++ js/myScript.js
@@ -1,9 +1,23 @@
-//div[@class='flex flex-wrap justify-center mx-auto max-width']
-
-
 const parentDiv = document.querySelector('div[class="flex flex-wrap justify-center mx-auto max-width"]');
-
 const div1Collection = Array.from(parentDiv.children).filter(child => child.tagName.toLowerCase() === 'div');
+const filteredCollection = [];
 
+div1Collection.forEach(div => {
+    const firstChildDiv = div.querySelector('div.sdh.col-12.relative.flex.flex-column');
+    if (firstChildDiv) {
+        const secondChildDiv = firstChildDiv.querySelector('div.flex.items-center.mx1-5.mt1-5.relative');
+        if (secondChildDiv) {
+            const thirdChildDiv = secondChildDiv.querySelector('div.icon-membership');
+            if (thirdChildDiv) {
+                const hiddenDiv = thirdChildDiv.querySelector('div[hidden]');
+                if (!(hiddenDiv && hiddenDiv.innerText.trim() === 'Standard Member')) {
+                    filteredCollection.push(div);
+                }
+            }
+        }
+    } else if (div.classList.contains('card-p')) {
+        filteredCollection.push(div);
+    }
+});
 
-console.log(div1Collection);
+console.log("Отфильтрованная коллекция:", filteredCollection);
